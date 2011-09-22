d8604ac96102dc2a56e2cfd93de51900ae8c9004
David Smith <codeactual@gmail.com>
2011-09-22 07:10:45 +0000

    +test skeletons

--- test/unit/FoxtrapTest.php
+++ test/unit/FoxtrapTest.php
@@ -4,8 +4,57 @@ use \Foxtrap\Factory;
 
 class FoxtrapTest extends PHPUnit_Framework_TestCase
 {
-  public function setUp()
+  /**
+   * @group cleansUp
+   * @test
+   */
+  public function cleansUp()
   {
-    parent::setUp();
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @group downloads
+   * @test
+   */
+  public function downloads()
+  {
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @group convertsDbRowToObj
+   * @test
+   */
+  public function convertsDbRowToObj()
+  {
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @group convertsJsonFileToArray
+   * @test
+   */
+  public function convertsJsonFileToArray()
+  {
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @group registersMarks
+   * @test
+   */
+  public function registersMarks()
+  {
+    $this->markTestIncomplete();
+  }
+
+  /**
+   * @group buildsJsonpCallback
+   * @test
+   */
+  public function buildsJsonpCallback()
+  {
+    $this->markTestIncomplete();
   }
 }
