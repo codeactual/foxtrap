fdd4f056861182e95aad188f1d8e716e2725379b
David Smith <codeactual@gmail.com>
2011-09-23 07:24:16 +0000

    Moved config.php to config/.

--- test/unit/bootstrap.php
+++ test/unit/bootstrap.php
@@ -3,4 +3,4 @@
 require __DIR__ . '/../../src/LoadClasses.php';
 require __DIR__ . '/TestData.php';
 
-define('FOXTRAP_CONFIG_FILE', __DIR__ . '/../../config.php');
+define('FOXTRAP_CONFIG_FILE', __DIR__ . '/../../config/config.php');
