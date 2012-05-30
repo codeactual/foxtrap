50dbc64b508938b6480a74049c91ef9032d77d35
David Smith <codeactual@gmail.com>
2012-05-30 02:28:33 +0000

    Replace layout with YUI grids.

--- pub/index.php
+++ pub/index.php
@@ -16,15 +16,19 @@ header('Content-type: text/html; charset=utf-8');
   <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 </head>
 <body>
-<div class="query-group">
-  <input id="q" type="text" value="<?php echo $q; ?>"/>
-</div>
-<div>
-  <div id="query-history" class="history-group float-left">&nbsp;</div>
-  <div id="results" class="results-group float-left">
-    <ul id="results-ac-output"></ul>
+<div class="yui3-g">
+  <div class="yui3-u-1-8">
+    <div id="logo">foxtrap</div>
+    <div id="query-history" class="history-group float-left">&nbsp;</div>
   </div>
-  <div class="float-clear"></div>
+  <div class="yui3-u-7-8">
+    <div class="query-group">
+      <input id="q" type="text" value="<?php echo $q; ?>"/>
+    </div>
+    <div id="results" class="results-group float-left">
+      <ul id="results-ac-output"></ul>
+    </div>
+  </div>
 </div>
 </body>
 </html>
